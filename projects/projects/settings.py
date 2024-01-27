884c764c0e0e1d384a2beeee7971fdfda1139644
Raphael Vigneault Ð <triangle1232009@hotmail.com>
2024-01-27 10:01:26 -0500

    project setup

--- projects/projects/settings.py
+++ projects/projects/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'app'
-    'api'
+    'oauth2_provider',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
