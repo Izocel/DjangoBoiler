cda4f9831b69d2f0b65d82f394b758e2f4f4328d
Raphael Vigneault Ð <triangle1232009@hotmail.com>
2024-01-26 19:00:58 -0500

    api

--- projects/projects/urls.py
+++ projects/projects/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("app.urls")),
+    path("api", include("api.urls")),
     path("admin", admin.site.urls),
     path("debug", include("debug_toolbar.urls")),
 ]
